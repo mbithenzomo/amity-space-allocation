c4fcf503cf0546e366e3aead177cf375d3d41de3
Mbithe Nzomo <mbithe.nzomo@gmail.com>
2016-05-04 14:44:59 +0300

    Add tests for rooms

--- tests/test_rooms.py
+++ tests/test_rooms.py
@@ -1,31 +1,30 @@
 import unittest
 from models.rooms import Room
-from models.persons import Person
 
 class TestRooms(unittest.TestCase):
 
-    # def set_office(self):
-    #     ''' Create a new office '''
-        
-
-    # def set_livingspace(self):
-    #     ''' Create a new living space '''
-    #     self.livingspace = Room("Emerald", "livingspace")
-
     def test_office_capacity(self):
-        ''' Check that capacity of office is 6 '''
-        office = Room("Oculus", "office")
+
+        ''' For office '''
+        office = Room("Oculus", "Office")
         result = office.get_capacity()
         self.assertEqual(6, result)
 
-    def test_livingspace_capacity(self):
-        ''' Check that capacity of living space is 4 '''
-        livingspace = Room("Emerald", "livingspace")
+        ''' For livingspace '''
+        livingspace = Room("Emerald", "Livingspace")
         result = livingspace.get_capacity()
         self.assertEqual(4, result)
 
-    # def test_assign_occupant(self):
-    #     ''' Check that assign_occupant function adds occupant to living space '''
-    #     self.occupant = Person("Mbithe", "Nzomo", "fellow")
-    #     self.livingspace.assign_occupant(self.occupant)
-    #     self.assertTrue(self.livingspace.is_occupied())
+    def test_room_type(self):
+
+        ''' For office '''
+        office = Room("Oculus", "Office")
+        result = office.get_room_type()
+        self.assertEqual("Office", result)
+
+        ''' For livingspace '''
+        livingspace = Room("Emerald", "Livingspace")
+        result = livingspace.get_room_type()
+        self.assertEqual("Livingspace", result)
+        
+        
