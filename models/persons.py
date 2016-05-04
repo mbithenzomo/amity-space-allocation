fd882b13c9a97e25054d27f775dff7d574c25e0b
Mbithe Nzomo <mbithe.nzomo@gmail.com>
2016-05-04 14:46:26 +0300

    Edit functions in Person class

--- models/persons.py
+++ models/persons.py
@@ -3,14 +3,22 @@ class Person(object):
     Creates a Person object.
     """
 
-    def __init__(self, fname, lname, job_type):
+    def __init__(self, fname, lname, job_type, wants_space="N"):
         """
         Initializing the class with a name
         """
         self.fname = fname
         self.lname = lname
         self.job_type = job_type
+        self.wants_space = wants_space
 
+    def get_person_details(self):
+        self.details = self.fname, self.lname, self.job_type, self.wants_space
+        return self.details
+
+    def get_job_type(self):
+        return self.job_type
+        
     def is_assigned():
         self.is_assigned = False # default value of is_assigned is False
         return self.is_assigned
