dcda2ddeab42b299774cd4368b73eda899c4d1dd
Mbithe Nzomo <mbithe.nzomo@andela.com>
2016-05-11 21:47:59 +0300

    Add error when creating rooms that already exist

--- models/amity.py
+++ models/amity.py
@@ -4,32 +4,45 @@ from people import Person
 
 spacer = "-" * 80
 
+
 class Amity():
 	def __init__(self):
 		self.rooms = []
 		self.offices = []
+		self.vacant_offices = []
 		self.livingspaces = []
+		self.vacant_livingspaces = []
 		self.people = []
 		self.fellows = []
+		self.assigned_fellows = []
 		self.staff = []
+		self.assigned_staff = []
 
 	def create_room(self, args):
 		"""Create new room(s)"""
 		print spacer
-		print "You have successfully added the following rooom(s):"
+		initial_count, final_count = len(self.rooms), 0
+		new_rooms = []
 		for room in args["<room_name>"]:
-			new_room = Room() # create new Room object
-			new_room.name = room # set room name
-			self.rooms.append(new_room) # add to list of rooms
+			new_room = Room()  # create new Room object
+			new_room.name = room  # set room name
+			for r in self.rooms:
+				if r.name == new_room.name:
+					print "A room you tried to create already exists! Please try again."
+					return
+			self.rooms.append(new_room)  # add to list of rooms
+			new_rooms.append(new_room)
 			if args["Office"]:
-				new_room.room_type = "Office" # set room type
-				new_room.capacity = 6 # set capacity
-				self.offices.append(new_room) # add to list of offices
+				new_room.room_type = "Office"  # set room type
+				new_room.capacity = 6  # set capacity
+				self.offices.append(new_room)  # add to list of offices
 			elif args["Living"]:
-				new_room.room_type = "Living" # set room type
-				new_room.capacity = 4 # set capacity
-				self.livingspaces.append(new_room) # add to list of living spaces
-			print "Name: " + ''.join(new_room.name) + " | Type: " + new_room.room_type
+				new_room.room_type = "Living"  # set room type
+				new_room.capacity = 4  # set capacity
+				self.livingspaces.append(new_room)  # add to list of living spaces
+		print "You have successfully added the following rooom(s):"
+		for new_room_ in new_rooms:
+			print "Name: " + ''.join(new_room_.name) + " | Type: " + new_room_.room_type
 		print spacer
 
 	def add_person(self, args):
@@ -37,21 +50,21 @@ class Amity():
 		print spacer
 		print "You have successfully added the following person:"
 		for person in args["<first_name>"]:
-			new_person = Person() # create new Person object
-			name = person + " " + args["<last_name>"] # set full name
-			job_type = "Staff" if args["Staff"] else "Fellow" # set job type
-			wants_space = "Yes" if args["<wants_space>"] is "Y" else "No" # set wants_space
-			self.people.append(new_person)# add to list of people
+			new_person = Person()  # create new Person object
+			name = person + " " + args["<last_name>"]  # set full name
+			job_type = "Staff" if args["Staff"] else "Fellow"  # set job type
+			wants_space = "Yes" if args["<wants_space>"] is "Y" else "No"  # set wants_space
+			self.people.append(new_person)  # add to list of people
 			print "Name: " + name + " | Job Type: " + job_type + \
-			" | Wants Space?:"  + " " + wants_space
+			" | Wants Space?:" + " " + wants_space
 			print spacer
 			"""Allocation to rooms"""
 			if job_type == "Staff":
-				self.staff.append(new_person) # add to list of staff
+				self.staff.append(new_person)  # add to list of staff
 			elif job_type == "Staff" and wants_space == "Yes":
 				if len(my_amity.offices) != 0:
 					office_choice = random.choice(my_amity.offices)
-					office_choice.occupants.append(new_person) # add to list of occupants for office
+					office_choice.occupants.append(new_person)  # add to list of occupants for office
 					print "You have successfully allocated " + name + \
 					" to the following office: " + office_choice.name
 				else:
@@ -61,7 +74,7 @@ class Amity():
 				if len(my_amity.livingspaces) != 0:
 					living_choice = random.choice(my_amity.livingspaces)
 					living_choice.occupants.append(new_person)
-					self.staff.append(new_person) # add to list of fellows
+					self.staff.append(new_person)  # add to list of fellows
 					print "You have successfully allocated " + name + \
 					" to the following living space: " + living_choice.name
 				else:
@@ -69,4 +82,4 @@ class Amity():
 					" cannot be assigned to one.\nAdd a new living space using the create_room command."
 		print spacer
 
-my_amity = Amity() # create new Amity object
+my_amity = Amity()  # create new Amity object
