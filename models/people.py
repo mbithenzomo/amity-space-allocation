bf47fb6dcb4d3926f8be30b9b11843cdd70496d6
Mbithe Nzomo <mbithe.nzomo@andela.com>
2016-05-11 16:18:50 +0300

    Rename persons.py to people.py; remove add_person function

---
+++ models/people.py
@@ -0,0 +1,17 @@
+import random
+from rooms import Room, Office, Living
+
+class Person(object):
+    """
+    Creates a Person object. Classes Fellow and Staff inherit from it.
+    """
+
+class Staff(Person):
+
+    job_type = "Staff"
+
+    ''' Allocation of Staff to Office '''
+
+class Fellow(Person):
+
+    job_type = "Fellow"
