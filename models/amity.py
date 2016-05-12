9798391af17e2401adab5a2c21add40301c845c2
Mbithe Nzomo <mbithe.nzomo@andela.com>
2016-05-12 15:02:31 +0300

    Add room allocation functionality

--- models/amity.py
+++ models/amity.py
@@ -1,85 +1,133 @@
 import random
-from rooms import Room
-from people import Person
+from rooms import Office, Living
+from people import Staff, Fellow
 
 spacer = "-" * 80
 
 
 class Amity():
-	def __init__(self):
-		self.rooms = []
-		self.offices = []
-		self.vacant_offices = []
-		self.livingspaces = []
-		self.vacant_livingspaces = []
-		self.people = []
-		self.fellows = []
-		self.assigned_fellows = []
-		self.staff = []
-		self.assigned_staff = []
+    def __init__(self):
+        self.rooms = []
+        self.offices = []
+        self.vacant_offices = []
+        self.livingspaces = []
+        self.vacant_livingspaces = []
+        self.people = []
+        self.fellows = []
+        self.assigned_fellows = []
+        self.staff = []
+        self.assigned_staff = []
 
-	def create_room(self, args):
-		"""Create new room(s)"""
-		print spacer
-		initial_count, final_count = len(self.rooms), 0
-		new_rooms = []
-		for room in args["<room_name>"]:
-			new_room = Room()  # create new Room object
-			new_room.name = room  # set room name
-			for r in self.rooms:
-				if r.name == new_room.name:
-					print "A room you tried to create already exists! Please try again."
-					return
-			self.rooms.append(new_room)  # add to list of rooms
-			new_rooms.append(new_room)
-			if args["Office"]:
-				new_room.room_type = "Office"  # set room type
-				new_room.capacity = 6  # set capacity
-				self.offices.append(new_room)  # add to list of offices
-			elif args["Living"]:
-				new_room.room_type = "Living"  # set room type
-				new_room.capacity = 4  # set capacity
-				self.livingspaces.append(new_room)  # add to list of living spaces
-		print "You have successfully added the following rooom(s):"
-		for new_room_ in new_rooms:
-			print "Name: " + ''.join(new_room_.name) + " | Type: " + new_room_.room_type
-		print spacer
+    def create_room(self, args):
+        """Create new room(s)"""
+        print spacer
+        new_rooms = []
+        for room in args["<room_name>"]:
+            if room.lower() in [r.name.lower() for r in self.rooms]:
+                print "One or more rooms you tried to create already exist! " \
+                    "Please try again."
+                return
+            if args["Office"]:
+                new_room = Office(room)
+                self.offices.append(new_room)
+            elif args["Living"]:
+                new_room = Living(room)
+                self.livingspaces.append(new_room)
+            self.rooms.append(new_room)
+            new_rooms.append(new_room)
+        print "You have successfully added the following rooom(s):"
+        for new_room_ in new_rooms:
+            print "Name: " + ''.join(new_room_.name) + " | Type: " \
+                + new_room_.room_type
+        print spacer
 
-	def add_person(self, args):
-		"""Add new person"""
-		print spacer
-		print "You have successfully added the following person:"
-		for person in args["<first_name>"]:
-			new_person = Person()  # create new Person object
-			name = person + " " + args["<last_name>"]  # set full name
-			job_type = "Staff" if args["Staff"] else "Fellow"  # set job type
-			wants_space = "Yes" if args["<wants_space>"] is "Y" else "No"  # set wants_space
-			self.people.append(new_person)  # add to list of people
-			print "Name: " + name + " | Job Type: " + job_type + \
-			" | Wants Space?:" + " " + wants_space
-			print spacer
-			"""Allocation to rooms"""
-			if job_type == "Staff":
-				self.staff.append(new_person)  # add to list of staff
-			elif job_type == "Staff" and wants_space == "Yes":
-				if len(my_amity.offices) != 0:
-					office_choice = random.choice(my_amity.offices)
-					office_choice.occupants.append(new_person)  # add to list of occupants for office
-					print "You have successfully allocated " + name + \
-					" to the following office: " + office_choice.name
-				else:
-					print "Unfortunately there are no offices in the system,\nso " + name + \
-					" cannot be assigned to one.\nAdd a new office using the create_room command."
-			elif job_type == "Fellow" and wants_space == "Yes":
-				if len(my_amity.livingspaces) != 0:
-					living_choice = random.choice(my_amity.livingspaces)
-					living_choice.occupants.append(new_person)
-					self.staff.append(new_person)  # add to list of fellows
-					print "You have successfully allocated " + name + \
-					" to the following living space: " + living_choice.name
-				else:
-					print "Unfortunately there are no living spaces in the system,\nso " + name + \
-					" cannot be assigned to one.\nAdd a new living space using the create_room command."
-		print spacer
+    def check_vacant_offices(self):
+        """Add vacant offices to list; remove full ones from list"""
+        for office in my_amity.offices:
+            if len(office.occupants) < office.capacity:
+                if office not in self.vacant_offices:
+                    self.vacant_offices.append(office)
+            elif len(office.occupants) >= office.capacity:
+                if office in self.vacant_offices:
+                    self.vacant_offices.remove(office)
 
-my_amity = Amity()  # create new Amity object
+    def check_vacant_livingspaces(self):
+        """Add vacant living spaces to list; remove full ones from list"""
+        for livingspace in my_amity.livingspaces:
+            if len(livingspace.occupants) < livingspace.capacity:
+                if livingspace not in self.vacant_livingspaces:
+                    self.vacant_livingspaces.append(livingspace)
+            elif len(livingspace.occupants) >= livingspace.capacity:
+                if livingspace in self.vacant_livingspaces:
+                    self.vacant_livingspaces.remove(livingspace)
+
+    def add_person(self, args):
+        """Add new person"""
+        print spacer
+        self.name = args["<first_name>"] + " " + args["<last_name>"]
+        self.wants_space = "Yes" if args["<wants_space>"] is "Y" else "No"
+        if args["Staff"]:
+            if self.wants_space == "Yes":
+                if my_amity.offices:
+                    my_amity.check_vacant_offices()
+                    if not self.vacant_offices:
+                        print "There are no vacant offices at this time."
+                        print "Please try again later."
+                        return
+                    office_choice = random.choice(self.vacant_offices)
+                    self.new_person = Staff(self.name)
+                    office_choice.occupants.append(self.new_person)
+                    self.staff.append(self.new_person)
+                    my_amity.success_added_person()
+                    print "You have successfully allocated " + self.name + \
+                        " of Employee ID " + str(self.new_person.emp_id) + \
+                        "\n to the following office: " + office_choice.name
+                    print spacer
+                else:
+                    print "There are no offices in the system."
+                    print "Add an office using the create_room command " \
+                        "and try again."
+                    return
+            else:
+                self.new_person = Staff(self.name)
+                my_amity.success_added_person()
+            self.staff.append(self.new_person)
+        elif args["Fellow"]:
+            if self.wants_space == "Yes":
+                if my_amity.livingspaces:
+                    my_amity.check_vacant_livingspaces()
+                    if not self.vacant_livingspaces:
+                        print "There are no vacant living spaces at this time."
+                        print "Please try again later."
+                        return
+                    living_choice = random.choice(self.vacant_livingspaces)
+                    self.new_person = Fellow(self.name)
+                    living_choice.occupants.append(self.new_person)
+                    self.fellows.append(self.new_person)
+                    my_amity.success_added_person()
+                    print "You have successfully allocated " + self.name + \
+                        " of Employee ID " + str(self.new_person.emp_id) + \
+                        "\nto the following living space: " + \
+                        living_choice.name
+                    print spacer
+                else:
+                    print "There are no living spaces in the system."
+                    print "Add a living space using the create_room command " \
+                        "and try again."
+                    return
+            else:
+                self.new_person = Fellow(self.name)
+                my_amity.success_added_person()
+            self.fellows.append(self.new_person)
+        self.people.append(self.new_person)
+
+    def success_added_person(self):
+        """Success message when person has been successfully added"""
+        print "You have successfully added the following person:"
+        print "Name: " + self.name + " | Employee ID: " + \
+            str(self.new_person.emp_id) + \
+            "\nJob Type: " + self.new_person.job_type + \
+            " | Wants Space?: " + self.wants_space
+        print spacer
+
+my_amity = Amity()
