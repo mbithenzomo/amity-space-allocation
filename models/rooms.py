5e100568405b19cdafabb59c7795c813340ea3a7
Mbithe Nzomo <mbithe.nzomo@andela.com>
2016-05-12 15:03:34 +0300

    Add __init__ functions to room classes

--- models/rooms.py
+++ models/rooms.py
@@ -1,21 +1,29 @@
-class Room():
+class Room(object):
     """
     Creates a Room object. Classes Office and Living inherit from it
     """
 
-    def __init__(self):
-        self.occupants = [] # list of occupants in room
+    def __init__(self, name=''):
+        self.occupants = []  # list of occupants in room
+        self.name = name
 
     def view_allocations(self, args):
         """ Prints all room allocations """
         pass
 
+    def __repr__(self):
+        return '<Room %s>' % self.name
+
+    @property
+    def room_type(self):
+        return self.__class__.__name__
+
 
 class Office(Room):
+    capacity = 6  # set capacity
 
-    capacity = 6
-
-
+    def __init__(self, name):
+        super(Office, self).__init__(name)
 
     def view_all_details(self):
         """ Prints details of all offices """
@@ -32,19 +40,26 @@ class Office(Room):
         pass
         # add person to office
 
+    def __repr__(self):
+        return '<Office %s>' % self.name
+
+
 class Living(Room):
 
-    capacity = 4
+    capacity = 4  # set capacity
+
+    def __init__(self, name):
+        super(Living, self).__init__(name)
 
     def view_unallocated(self):
         """ Prints all offices with free spaces """
 
-        spaces = capacity - len(occupants) # get number of spaces available
-
-        print "The following living spaces have free spaces: "
-        for livingspace in livingspaces:
-            if spaces != 0:
-                print  "Name: " + ''.join(room) + " | Spaces available: " + spaces
+        # spaces = capacity - len(occupants) # get number of spaces available
+        #
+        # print "The following living spaces have free spaces: "
+        # for livingspace in livingspaces:
+        #     if spaces != 0:
+        #         print  "Name: " + ''.join(room) + " | Spaces available: " + spaces
 
     def view_all_details(self):
         """ Prints details of all living spaces """
