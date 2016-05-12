264cb96109152d9ceedd035d262db7e9b3930246
Mbithe Nzomo <mbithe.nzomo@andela.com>
2016-05-12 15:03:23 +0300

    Add __init__ functions to people classes

--- models/people.py
+++ models/people.py
@@ -1,17 +1,23 @@
-import random
-from rooms import Room, Office, Living
-
 class Person(object):
     """
     Creates a Person object. Classes Fellow and Staff inherit from it.
     """
+    def __init__(self, name):
+        self.name = name
+        self.emp_id = id(self)
+
 
 class Staff(Person):
 
     job_type = "Staff"
 
-    ''' Allocation of Staff to Office '''
+    def __init__(self, name):
+        super(Staff, self).__init__(name)
+
 
 class Fellow(Person):
 
     job_type = "Fellow"
+
+    def __init__(self, name):
+        super(Fellow, self).__init__(name)
